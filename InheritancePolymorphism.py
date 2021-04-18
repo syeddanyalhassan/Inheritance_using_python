beb9c81f2200aee021246dbd1dce35edf0ab548f
syeddanyalhassan <syeddanyalhassan@gmail.com>
2021-04-18 17:21:44 +0500

    New filed added

--- InheritancePolymorphism.py
+++ InheritancePolymorphism.py
@@ -43,19 +43,50 @@ class Person:
 
       else:
         print("{} is unconscious.".format(self.firstname))
+        
+        
+class Enemy(Person):
+    def __init__(self, weapon, firstname, lastname, health, status):
+        super().__init__(firstname, lastname, health, status)
+        self.weapon = weapon
+        
+    def hurt(self, other):
+
+        if self.weapon == 'rock':
+           other.health -= 10
+
+        elif self.weapon == 'stick':
+           other.health -= 5
+
+        print(other.health)
     
+    def insult(self, other):
+        if other.health <= 80:
+            print("{}, you are tired and weak".format(other.firstname))    
+    
+    def steal(self, other):
+        print("ha ha ha, {}, I have your stuff!".format(other.firstname))
+
+        if other.status == True:
+            other.status = False    
+    
+
+
+  
 Maria = Person("Maria", "Gutierrez", 95, status=True)
 Rey = Person("Rey", "Jones", 88, status=False)
 Lee = Person("Lee", "Williams", 72, status=True)   
-
+Alex = Enemy('rock', 'Alex', 'Wayne', 75, status = False)    
 print("{} is my friend? {}".format(Maria.firstname, Maria.status))
 print("{} is my friend? {}".format(Rey.firstname, Rey.status))
 print("{} is my friend? {}".format(Lee.firstname, Lee.status))
-
 Maria.introduce()
 Rey.introduce()
 Lee.introduce()
-
 Maria.status_change()
 Rey.status_change()
 Lee.status_change()
+Alex.hurt(Maria)
+Alex.insult(Rey)
+Alex.insult(Lee)
+Alex.steal(Rey)
